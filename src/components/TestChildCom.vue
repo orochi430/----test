851eb377e4be471d0d1bcf2a1ee484e53c291364
陆海云 <luhy@dream-it.cn>
2024-07-22 18:12:02 +0800

    测试测试

--- src/components/TestChildCom.vue
+++ src/components/TestChildCom.vue
@@ -1,35 +1,46 @@
 <template>
   <div>
     <div>childCom</div>
-    <div>{{myTitle}}</div>
+    <div>{{ myTitle }}</div>
     <div v-if="isPublished">已发布</div>
     <div v-else>未发布</div>
-  <div>{{myAuthor.age}}{{myAuthor.name}}  </div>
-  <van-button @click="$emit(`save-form`)">保存</van-button>
-    <slot></slot>
+    <div>{{ myAuthor.age }}{{ myAuthor.name }}</div>
+    <van-button @click="$emit(`save-form`)">保存</van-button>
+    <van-button @click="updateNoteTitle">修改</van-button>
+    {{ noteTitle }}
+    <div>
+      <slot name="header">头部内容</slot>
+      <slot name="footer">底部内容</slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    props:{
-        myTitle:String,
-        isPublished:Boolean,
-        author:Object,
-    },
-    data(){
-      return{
-        myAuthor:this.author
-      }
-    },
-    mounted(){
-      console.log('tag', this.author);
-      this.myAuthor.age=111;
-      this.isPublished=false;
+  props: {
+    myTitle: String,
+    isPublished: Boolean,
+    author: Object
+  },
+  methods: {
+    updateNoteTitle() {
+      this.noteTitle = "小说2"
+      this.$emit("update:noteTitle", this.noteTitle)
     }
+  },
+  data() {
+    return {
+      myAuthor: this.author,
+      noteTitle: "小说1"
+    }
+  },
+  mounted() {
+    console.log("tag", this.author)
+    this.myAuthor.age = 111
+    this.isPublished = false
+  }
 }
 </script>
 
 <style>
-
 </style>
--- src/components/testCom.vue
+++ src/components/testCom.vue
@@ -1,38 +1,44 @@
 <template>
   <div>
     mytest
-    <TestChildCom v-on:save-form="saveForm" :author="childData.author" :my-title="`myTitle123`" :is-published="childData.isPublished">123</TestChildCom>
+    <TestChildCom v-on:save-form="saveForm" :author="childData.author" :my-title="`myTitle123`" :is-published="childData.isPublished">
+      <template v-slot:header>
+        <van-button>{{ childData.author.name }}</van-button>
+      </template>
+        <template v-slot:footer>
+        <van-button>{{ Date.now() }}</van-button>
+      </template>
+    </TestChildCom>
   </div>
 </template>
 
 <script>
-import TodoCenter from "@/components/documentCenter/TodoCenter";
-import TestChildCom from '@/components/TestChildCom';
+import TodoCenter from "@/components/documentCenter/TodoCenter"
+import TestChildCom from "@/components/TestChildCom"
 export default {
-    components:{
-        TestChildCom,
-        TodoCenter
-    },
-    methods:{
-      saveForm(){
-        alert(`save`)
-      }
-    },
-    data(){
-      return{
-        childData:{
-          isPublished:true,
-          title:"childTitle",
-          author:{
-            age:14,
-            name:"张三"
-          }
+  components: {
+    TestChildCom,
+    TodoCenter
+  },
+  methods: {
+    saveForm() {
+      alert(`save`)
+    }
+  },
+  data() {
+    return {
+      childData: {
+        isPublished: true,
+        title: "childTitle",
+        author: {
+          age: 14,
+          name: "张三"
         }
       }
     }
+  }
 }
 </script>
 
 <style>
-
 </style>
