ce40eef8972443de82dc6d7e23ba38408c23eb34
陆海云 <luhy@dream-it.cn>
2024-07-22 15:37:57 +0800

    测试测试

---
+++ src/components/TestChildCom.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>childCom</div>
+    <div>{{myTitle}}</div>
+    <div v-if="isPublished">已发布</div>
+    <div v-else>未发布</div>
+  <div>{{myAuthor.age}}{{myAuthor.name}}  </div>
+  <van-button @click="$emit(`save-form`)">保存</van-button>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        myTitle:String,
+        isPublished:Boolean,
+        author:Object,
+    },
+    data(){
+      return{
+        myAuthor:this.author
+      }
+    },
+    mounted(){
+      console.log('tag', this.author);
+      this.myAuthor.age=111;
+      this.isPublished=false;
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/testCom.vue
+++ src/components/testCom.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
-    mytestCom
+    mytest
+    <TestChildCom v-on:save-form="saveForm" :author="childData.author" :my-title="`myTitle123`" :is-published="childData.isPublished">123</TestChildCom>
   </div>
 </template>
 
 <script>
+import TodoCenter from "@/components/documentCenter/TodoCenter";
+import TestChildCom from '@/components/TestChildCom';
 export default {
-
+    components:{
+        TestChildCom,
+        TodoCenter
+    },
+    methods:{
+      saveForm(){
+        alert(`save`)
+      }
+    },
+    data(){
+      return{
+        childData:{
+          isPublished:true,
+          title:"childTitle",
+          author:{
+            age:14,
+            name:"张三"
+          }
+        }
+      }
+    }
 }
 </script>
 
